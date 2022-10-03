8105e3d2f6f3dbbfbefdb1b7f42c1e5b989d4332
Alaysia Veal <alaysiaveal@gmail.com>
2022-10-03 15:28:19 -0400

    add css styling on both html files

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-document.querySelector('#play-game')
-document.addEventListener('click', function () {
-  document.getElementById('play-game').href = 'game.html'
+const dice = document.getElementsByClassName('first-dice')
+const colors = ['blue', 'green', 'purple', 'pink']
+
+dice.addEventListener('click', function onClick() {
+  dice.style.backgroundColor = colors[index]
 })
