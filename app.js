0171a2f39e83beef692030a51098ab28f9fffa49
Alaysia Veal <alaysiaveal@gmail.com>
2022-10-04 15:24:18 -0400

    add event listener to dice

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+const dice = document.querySelector('.dice')
+const colors = ['lightskyblue', 'plum', 'palevioletred', 'lightgoldenrodyellow']
+let color = 0
 
+dice.addEventListener('click', function onClick() {
+  dice.style.backgroundColor = colors[color]
+  color = color >= colors.length - 1 ? 0 : color + 1
+})
