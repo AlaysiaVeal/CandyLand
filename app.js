eae1e7fe9a0614c5f28800e81c45029de3b944d4
Alaysia Veal <alaysiaveal@gmail.com>
2022-10-04 12:30:42 -0400

    game board and boxes

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-const dice = document.getElementsByClassName('first-dice')
-const colors = ['blue', 'green', 'purple', 'pink']
 
-dice.addEventListener('click', function onClick() {
-  dice.style.backgroundColor = colors[index]
-})
