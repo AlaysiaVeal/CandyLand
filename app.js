017f74515dd477075849be80ca6413a84521f1f0
Alaysia Veal <alaysiaveal@gmail.com>
2022-10-06 11:45:07 -0400

    add event listener on first game piece

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 const dice = document.querySelector('.dice')
 const dice2 = document.querySelector('.dice2')
 const colors = ['lightskyblue', 'plum', 'palevioletred', 'lightgoldenrodyellow']
-const boxes = document.querySelectorAll('.box')
+const boxes = document.querySelectorAll('.cell')
 const colors2 = ['#CCFFFF', '#663300', '#000000', '#FFFFFF']
+const cell = document.querySelectorAll('.cell')
+const gameBoard = [
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  ''
+]
 
+let player1 = 0
+let gamepiece1 = document.querySelector('.gumDrop')
+let player2 = 0
+let gamepiece2 = document.querySelector('.bubbleGum')
+let gamepieces = document.querySelectorAll('.game-pieces')
 let color = 0
+let turn = true
+let currentPlayer = 0
 
 dice.addEventListener('click', function onClick() {
   dice.style.backgroundColor = colors[color]
@@ -19,28 +55,13 @@ dice2.addEventListener('click', function onClick() {
 boxes.forEach((box) => {
   box.addEventListener('click', function onClick() {
     boxes.forEach((el) => {
-      if (el.style.backgroundColor === colors[color]) {
-        el.style.backgroundColor = 'white'
+      if (el.style.backgroundColor === 'white') {
+        el.style.backgroundColor = colors[Math.floor(Math.random() * 4)]
+      }
+      if (el.classList.contains('gumDrop')) {
+        el.classList.remove('gumDrop')
       }
     })
-    box.style.backgroundColor = 'white'
+    box.classList.toggle('gumDrop')
   })
 })
-/* boxes.forEach((box) => {
-  box.addEventListener('click', function onClick() {
-    boxes.forEach((el) => {
-      if (el.style.backgroundColor === 'white') {
-        el.style.backgroundColor = 'black'
-      }
-    })
-    box.style.backgroundColor = 'white'
-  })
-}) */
-
-/*   */
-
-/*  {
-    boxes.forEach((el) => {
-      if (el.style.backgroundColor === 'white') {
-        el.style.backgroundColor = 'black'
-      } */
